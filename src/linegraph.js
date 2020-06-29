f3a96959d16454958880322c503f02260f920ca1
ankitOneNineNineNine <pradhanankit12@gmail.com>
2020-06-29 08:24:02 +0545

    changes

--- src/linegraph.js
+++ src/linegraph.js
@@ -45,7 +45,7 @@ export default class Linegraph extends React.Component {
           },
           {
             label: "RECOVERED",
-            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
             data: this.props.recovered,
           },
         ],
